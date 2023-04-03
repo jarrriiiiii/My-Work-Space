e963ddd72794770ef9fa3afc6f00b2abc8ec9815
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-04-03 14:59:52 +0500

    Create Get-Data-From-DB-Table.ts

---
+++ Get-Data-From-DB-Table.ts
@@ -0,0 +1,32 @@
+//to get all the data from the db table
+
+
+  async getPopUpFormData(): Promise<ResponseDto> {
+    try {
+      const getPopUp = getRepository(PopupForm);
+      const result =  getPopUp.createQueryBuilder('pop')
+      const data = await result.getMany();
+      return { message: commonMessage.get, data: data };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// to get all the list of admin roles from the db
+
+  async getRoleList():Promise<ResponseDto> {
+    try {
+      const adminRole = getRepository(AdminRole)
+      const Result = adminRole.createQueryBuilder('role')
+      const totalItems = await Result.getMany();
+      return { message: commonMessage.get, data: totalItems };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
