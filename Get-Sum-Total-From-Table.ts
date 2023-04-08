f59669cac57f1fbaf2534ce10bf03531955f0e58
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-04-08 22:35:35 +0500

    4 Code Blocks

--- Get-Sum-Total-From-Table.ts
+++ Get-Sum-Total-From-Table.ts
@@ -96,6 +96,24 @@ async salesQuotation() {
     }
   }
   
+
+///////////////////////////////////////CODE 4////////////////////////////////////////////////
+//This code retrieve the total sum of the 'noOfUnit' column in the 'Inventory' table.
+
+    
+  async getNoOfUnits() {
+    try{
+      const units = getRepository(Inventory).createQueryBuilder('units');
+      const getAll = await units.select(['SUM(units.noOfUnit)']) //.getCount()
+
+      const data = await getAll.getRawOne();
+
+
+      return {message: commonMessage.get , data : {NoOfUnits : data}}
+    }catch(error){  
+      throw new InternalServerErrorException(error);
+    }
+  }
   
   
   
