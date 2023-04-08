2e478bd0f662fd9adbe14cfc57a61f746207a04a
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-04-08 22:35:44 +0500

    Update Temp-Junk-Codes.ts

--- Temp-Junk-Codes.ts
+++ Temp-Junk-Codes.ts
@@ -1,20 +1,4 @@
-///////////////////////////////////////CODE 6////////////////////////////////////////////////
-//This code retrieve the total sum of the 'noOfUnit' column in the 'Inventory' table.
 
-    
-  async getNoOfUnits() {
-    try{
-      const units = getRepository(Inventory).createQueryBuilder('units');
-      const getAll = await units.select(['SUM(units.noOfUnit)']) //.getCount()
-
-      const data = await getAll.getRawOne();
-
-
-      return {message: commonMessage.get , data : {NoOfUnits : data}}
-    }catch(error){  
-      throw new InternalServerErrorException(error);
-    }
-  }
 
 
 
