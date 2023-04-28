a523022dcebc489e7d2232758f558e76ab6bbca5
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-04-28 17:25:44 +0500

    1 Code Blocks

---
+++ Date-After-One-Month.ts
@@ -0,0 +1,5 @@
+      const date = new Date();
+      const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+      
+      
+      //For example, if the current date is April 28, 2023, then date would represent April 28, 2023 and endDate would represent May 31, 2023 (since the next month after April is May, and May has 31 days).
