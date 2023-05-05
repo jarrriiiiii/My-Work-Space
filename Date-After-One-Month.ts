773702ff08711b141a133397e8e3f10790d3b689
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-05 13:19:36 +0500

    Delete Date-After-One-Month.ts

--- Date-After-One-Month.ts
+++
@@ -1,9 +0,0 @@
-//Retrieve, get date after one month, date function, next month. For example, if the current date is April 28, 2023, then date would represent April 28, 2023 and endDate would represent May 31, 2023 (since the next month after April is May, and May has 31 days).
-
-
-
-const date = new Date();
-const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-      
-      
-   
