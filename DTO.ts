2fd2ced2732b5107197aa6387155589d006fcec2
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-05 17:07:58 +0500

    Tagged

--- DTO.ts
+++ DTO.ts
@@ -1,6 +1,3 @@
-import { ApiProperty } from "@nestjs/swagger"
-import { IsBoolean, IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber, IsString } from "class-validator"
-
 export class CreatePropertyWalletProductStep1Dto {
 
     @ApiProperty()
@@ -91,96 +88,6 @@ export class CreatePropertyWalletProductStep1Dto {
     @IsNotEmpty()
     longitude : string
 
-}
-
-
-
-
-
-
-
-
-
-
-/////////////////
-
-
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropertyWalletInventoryDto, CreatePropertyWalletInventoryStep2Dto } from './create-property_wallet_inventory.dto';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsNotEmpty, IsBoolean, IsString, IsOptional } from 'class-validator';
-
-export class UpdatePropertyWalletInventoryDto extends PartialType(CreatePropertyWalletInventoryDto) {
-
-
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    projectTypeId: number
-
-
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    projectSubTypeId: number 
-
-    @ApiProperty()
-    @IsBoolean()
-    @IsOptional()
-    NOC : boolean
-
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    landSize: number
-
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    landAreaId: number 
-
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    description : string
-
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    price : number
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    cashDealCommissionAmount : string
-
-
-    @ApiProperty()
-    @IsString()
-    @IsOptional()
-    InstallmentDealCommissionAmount : string
-
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    minimumPrice : number
-
-}
-
-
-
-
-
-////////////////
-
-
-
-
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, Matches, MaxLength, MinLength} from "class-validator";
-
-export class CreateAdminUserByRoleDto {
     @ApiProperty({ required: true })
     @IsString()
     fullName: string;
@@ -195,7 +102,6 @@ export class CreateAdminUserByRoleDto {
     @ApiProperty({ required: true })
     @IsPhoneNumber()
     phone: string
-  
 }
 
 
