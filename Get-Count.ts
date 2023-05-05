e3bd0922ea3c34693b65ea0ec0a6449ae0508ce1
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-05 12:19:50 +0500

    x

--- Get-Count.ts
+++ Get-Count.ts
@@ -1,10 +1,5 @@
-///////////////////////////////////////CODE 1////////////////////////////////////////////////
-// This is a function to get the count of all agencies in a database. 
-
-// The function first creates a query builder instance for the Agency entity, and aliases it as agency. 
-// It then selects the id column from the agency table and calls the getCount() method on the query builder instance. 
-// This method executes the query and returns the total count of all rows in the agency table.
-// Finally, the function returns an object with a success message and a data property containing the count of all agencies as AllAgency. 
+////////////////////////////////////////////////////////////////////////////////////
+//get count, total number, serial number, number of entries, objects, items in table database db entities entity, getCount
 
 async allAgenciesCount() {
     try{
@@ -18,13 +13,8 @@ async allAgenciesCount() {
       throw new InternalServerErrorException(error);
     }
   }
-
-
-
-///////////////////////////////////////CODE 2////////////////////////////////////////////////
-// This function is retrieving the: 
-// Total count of FinalizeSale entities in the database
-// Count of FinalizeSale entities that were created within the last 24 hours.
+//////////////////////////////////////////////////////////////////////////////////////
+//get count, total number, serial number, number of entries, objects, items in table database db entities entity, getCount, within the last 24 hours hrs, specific particular time range, limited time span, time interval
 
     async getTotalSaleOrder() {
       try{
@@ -40,29 +30,9 @@ async allAgenciesCount() {
       }
     }
 
-///////////////////////////////////////CODE 3////////////////////////////////////////////////
-// This is a function to get the count of all agencies created within the last 24 hours. 
-
-// The function first creates a query builder instance for the Agency entity, and aliases it as agency. 
-// It then selects the id column from the agency table and uses the where() method to filter the results based on the createdAt column of the Agency entity. 
-// The createdAt column is compared to the current date and time minus 24 hours using the NOW() and INTERVAL functions respectively. 
-// The getCount() method is called on the query builder instance, which executes the query and returns the count of all rows in the agency table that meet the specified criteria. 
-// Finally, the function returns an object with a success message and a data property containing the count of agencies created within the last 24 hours as AllAgency. 
-
-
-async agenciesLastDayCount() {
-    try{
-      const NoOfagency = getRepository(Agency).createQueryBuilder('agency');
-      const result = await NoOfagency.select(['agency.id']).where("agency.createdAt >= NOW() - INTERVAL '24 HOUR'").getCount()
-      
-      return {message: commonMessage.get , data : {AllAgency : result}}
-    }catch(error){
-      throw new InternalServerErrorException(error);
-    }
-  }
-
-///////////////////////////////////////CODE 4////////////////////////////////////////////////
-// The code you provided is using TypeORM's QueryBuilder to create a query for selecting the count of users who are verified and were created in the last 24 hours.
+/////////////////////////////////////////////////////////////////////////////////////
+//Code is selecting the count of users who are verified and were created in the last 24 hours.
+////get count, total number, serial number, number of entries, objects, items in table database db entities entity, getCount, within the last 24 hours hrs, specific particular time range, limited time span, time interval
 
     async getLastRegisteredUsers() {
     try{
@@ -80,14 +50,11 @@ async agenciesLastDayCount() {
     }
   }
 
-///////////////////////////////////////CODE 5////////////////////////////////////////////////
-// This code retrieves the count of verified users in a database using TypeORM, and returns a JSON object containing the count.
+///////////////////////////////////////////////////////////////////////////////////////
+////get count, total number, serial number, number of entries, objects, items in table database db entities entity, getCount, within the last 24 hours hrs, specific particular time range, limited time span, time interval
 
-// It starts by creating a query builder using getRepository(User).createQueryBuilder('user'), which creates a query builder for the User entity in the database.
-// It then selects the count of user IDs using user.select(['COUNT(user.id)']).
-// It filters the results by adding a WHERE clause using user.where('user.isVerified = :verify' , {verify : true}), which selects only verified users.
-// It executes the query using result.getCount() and assigns the result to the data variable.
-// Finally, it returns a JSON object containing the count.
+// It then selects the count of user IDs using user.select(['COUNT(user.id)']). It filters the results by adding a WHERE clause using user.where('user.isVerified = :verify' , {verify : true}), which selects only verified users.
+
 
 
   async getRegisteredUsers():Promise<ResponseDto> {
@@ -104,23 +71,16 @@ async agenciesLastDayCount() {
     }
   }
 
+//////////////////////////////////////////////////////////////////////////////////
+//get count, total number, serial number, number of entries, objects, items in table database db entities entity, getCount, within the last 24 hours hrs, specific particular time range, limited time span, time interval
+//The first query counts all users with roles of 'agentManager' or 'agentStaff'. The second query counts all users with roles of 'agentManager' or 'agentStaff' that were created within the last 24 hours. leftJoinAndSelect
   
-
-///////////////////////////////////////CODE 6////////////////////////////////////////////////
-//The first query counts all users with roles of 'agentManager' or 'agentStaff'. 
-//The second query counts all users with roles of 'agentManager' or 'agentStaff' that were created within the last 24 hours.
 
   async getNoOfRegisteredStaff() {
       try{
         
-//This line creates a repository for the User entity, which allows us to perform database operations on it.
       const userRepo = getRepository(User)
-
-// This line creates a new query builder instance and sets the alias for the User entity to u
       const result = await userRepo.createQueryBuilder('u')
-
-
-//This select the count of user IDs, which will be returned as the result of the query.
       .select('COUNT(u.id)') 
 
 
@@ -153,8 +113,9 @@ async agenciesLastDayCount() {
   }
   
 
-///////////////////////////////////////CODE 7////////////////////////////////////////////////
-// This function is designed to retrieve the total count of units in an inventory, as well as the number of units added in the last 24 hours.
+/////////////////////////////////////////////////////////////////////////////////////
+////get count, total number, serial number, number of entries, objects, items in table database db entities entity, getCount, within the last 24 hours hrs, specific particular time range, limited time span, time interval, retrieve the total count of units in an inventory, as well as the number of units added in the last 24 hours.
+
 async getNoOfUnits() {
     try{
       const units = getRepository(Inventory).createQueryBuilder('units');
