228a2a8a004063f653adb4a2e3f02a085f247486
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-06 16:50:55 +0500

    Delete property-wallet-product directory

--- property-wallet-product/dto/update-property-wallet-product.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropertyWalletProductStep1Dto, CreatePropertyWalletProductStep2Dto } from './create-property-wallet-product.dto';
-
-export class UpdatePropertyWalletProductStep1Dto extends PartialType(CreatePropertyWalletProductStep1Dto) {}
-export class UpdatePropertyWalletProductStep2Dto extends PartialType(CreatePropertyWalletProductStep2Dto) {}
