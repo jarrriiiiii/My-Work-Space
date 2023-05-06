d921b1c2d14a4f4e13c9ef1e33c13708511a45f5
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-06 16:27:39 +0500

    Untagged

--- POST - GET - DELETE.ts
+++ POST - GET - DELETE.ts
@@ -1,23 +1,8 @@
-  @Post('createUtil')
-  @hasModulePermission(moduleType.inventories)
-  createUtil(@Body() createPropertyWalletUtilDto: CreatePropertyWalletUtilDto) {
-    return this.propertyWalletUtilsService.createUtil(createPropertyWalletUtilDto);
-  }
-
   @Get('getUtil/:propertyWalletInventoryId')
   @hasModulePermission(moduleType.inventories)
   getUtil(@Param('propertyWalletInventoryId') propertyWalletInventoryId: string) {
     return this.propertyWalletUtilsService.getUtil(+propertyWalletInventoryId);
   }
-
-  @Delete('removeUtil')
-  @hasModulePermission(moduleType.inventories)
-  removeUtil(createPropertyWalletUtilDto: CreatePropertyWalletUtilDto) {
-    return this.propertyWalletUtilsService.removeUtil(createPropertyWalletUtilDto);
-  }
-
-  
-}
 
 ////
 
@@ -31,28 +16,7 @@ export class PropertyWalletUtilsService {
     )  {}
 
 
-  async createUtil(createPropertyWalletUtilDto: CreatePropertyWalletUtilDto): Promise<ResponseDto> {
-    const queryRunner = this.connection.createQueryRunner();
-    await queryRunner.connect()
-    await queryRunner.startTransaction()
-  
-    try {
-      const repo = queryRunner.manager.getRepository(PropertyWalletMultiUtilities);
-  
-      const userId = await this.adminAuth.getAdminUserId()
-      createPropertyWalletUtilDto.createdByAdmin = userId
-      const result = await repo.save(createPropertyWalletUtilDto)
-      await queryRunner.commitTransaction()
-      return { message: commonMessage.create, data: {result} };
-    } 
-    catch (error) {
-      await queryRunner.rollbackTransaction();
-      throw new InternalServerErrorException(error);
-    }
-     finally {
-      await queryRunner.release();
-    }
-    }
+
 
 
     async getUtil(propertyWalletInventoryId :number): Promise<ResponseDto> {
@@ -88,29 +52,6 @@ export class PropertyWalletUtilsService {
         };
       } catch (error) {
         throw new InternalServerErrorException(error);
-      }
-    }
-
-
-    async removeUtil(createPropertyWalletUtilDto: CreatePropertyWalletUtilDto): Promise<ResponseDto> {
-      const queryRunner = this.connection.createQueryRunner();
-      await queryRunner.connect()
-      await queryRunner.startTransaction()
-      try {
-        const PWMURepo = queryRunner.manager.getRepository(PropertyWalletMultiUtilities);
-        await PWMURepo.delete({
-          propertyWalletInventoryId : createPropertyWalletUtilDto.propertyWalletInventoryId,
-          propertyWalletUtilId : createPropertyWalletUtilDto.propertyWalletUtilId
-        }) 
-        await queryRunner.commitTransaction()
-        return { message: commonMessage.delete, data: null }
-      }
-      catch (error) {
-        await queryRunner.rollbackTransaction()
-        throw new InternalServerErrorException(error)
-      }
-      finally {
-        await queryRunner.release()
       }
     }
 
