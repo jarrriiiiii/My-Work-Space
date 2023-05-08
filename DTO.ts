7ad01ce579bfda525d09367659eaa86e2156f063
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-08 19:33:26 +0500

    Tagged

--- DTO.ts
+++ DTO.ts
@@ -5,37 +5,15 @@ export class CreatePropertyWalletProductStep1Dto {
     @IsNotEmpty()
     projectTypeId: number
 
-
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    projectSubTypeId: number 
-
     @ApiProperty()
     @IsBoolean()
     @IsNotEmpty()
     NOC : boolean
 
     @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    landSize: number
-
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    landAreaId: number 
-
-
-    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     description : string
-
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    ownerName : string
 
     @ApiProperty()
     @IsPhoneNumber()
@@ -47,53 +25,9 @@ export class CreatePropertyWalletProductStep1Dto {
     @IsNotEmpty()
     ownerEmail : string
     
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    price : number
-
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    cashDealCommissionAmount : string
-
-
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    InstallmentDealCommissionAmount : string
-
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    minimumPrice : number
-
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    address : string
-
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    city : string
-
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    latitude : string
-
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    longitude : string
-
     @ApiProperty({ required: true })
     @IsString()
     fullName: string;
-  
-    @ApiProperty({ required: true })
-    adminRoleId: number
 
     @ApiProperty({ required: true })
     @IsEmail()
