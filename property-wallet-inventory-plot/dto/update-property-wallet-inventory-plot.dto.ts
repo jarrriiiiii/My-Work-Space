75ea6c04200e97c96b3cc3f9ba8ab775c675dc70
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-08 19:35:28 +0500

    Delete property-wallet-inventory-plot directory

--- property-wallet-inventory-plot/dto/update-property-wallet-inventory-plot.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropertyWalletInventoryPlotDto } from './create-property-wallet-inventory-plot.dto';
-
-export class UpdatePropertyWalletInventoryPlotDto extends PartialType(CreatePropertyWalletInventoryPlotDto) {}
