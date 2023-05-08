87a839d6e8270d38fe1c8133990bcd41bf747a2c
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-08 11:58:46 +0500

    Delete temp/property-wallet-facing directory

--- temp/property-wallet-facing/dto/update-property-wallet-facing.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropertyWalletFacingDto } from './create-property-wallet-facing.dto';
-
-export class UpdatePropertyWalletFacingDto extends PartialType(CreatePropertyWalletFacingDto) {}
