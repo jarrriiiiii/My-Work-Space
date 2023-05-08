2091be3b41a2a481b7d011d1f6f41685ce4344b6
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-08 11:58:11 +0500

    Delete temp/property-wallet-utils directory

--- temp/property-wallet-utils/dto/update-property-wallet-util.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropertyWalletUtilDto } from './create-property-wallet-util.dto';
-
-export class UpdatePropertyWalletUtilDto extends PartialType(CreatePropertyWalletUtilDto) {}
