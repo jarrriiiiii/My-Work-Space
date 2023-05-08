b1babf17c3258a8592796515f1f539c9e56f2c41
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-08 11:59:49 +0500

    Delete temp directory

--- temp/property-wallet-product-facing/dto/update-property-wallet-product-facing.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropertyWalletProductFacingDto } from './create-property-wallet-product-facing.dto';
-
-export class UpdatePropertyWalletProductFacingDto extends PartialType(CreatePropertyWalletProductFacingDto) {}
--- temp/property-wallet-product-facing/property-wallet-product-facing.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PropertyWalletProductFacingService } from './property-wallet-product-facing.service';
-import { PropertyWalletProductFacingController } from './property-wallet-product-facing.controller';
-
-@Module({
-  controllers: [PropertyWalletProductFacingController],
-  providers: [PropertyWalletProductFacingService]
-})
-export class PropertyWalletProductFacingModule {}
