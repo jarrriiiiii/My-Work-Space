3b1a0bd367b449868136ee297761147365ad9bef
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-05-08 11:59:18 +0500

    Delete temp/property-wallet-product-utils directory

--- temp/property-wallet-product-utils/dto/update-property-wallet-product-util.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropertyWalletProductUtilDto } from './create-property-wallet-product-util.dto';
-
-export class UpdatePropertyWalletProductUtilDto extends PartialType(CreatePropertyWalletProductUtilDto) {}
--- temp/property-wallet-product-utils/property-wallet-product-utils.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PropertyWalletProductUtilsService } from './property-wallet-product-utils.service';
-import { PropertyWalletProductUtilsController } from './property-wallet-product-utils.controller';
-
-@Module({
-  controllers: [PropertyWalletProductUtilsController],
-  providers: [PropertyWalletProductUtilsService]
-})
-export class PropertyWalletProductUtilsModule {}
