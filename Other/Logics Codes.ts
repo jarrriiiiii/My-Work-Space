750d70336e227d4011f0bd081be7c5e482b86cd4
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-06-19 17:57:34 +0500

    Tagged

--- Other/Logics Codes.ts
+++ Other/Logics Codes.ts
@@ -1,3 +1,4 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //This code is checking for different properties in different tables based on the userId retrieved from this.authService.getUserId(). If a property exists for a user in a particular table, then the corresponding property in the obj1 object is set to true if found if not found set to false. object, boolean,
 
 async agencyLogoCheck(){
@@ -57,7 +58,14 @@ throw new InternalServerErrorException(error);
 }
 
 
-////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //checks if a user already exists in the database, table, db by email, phone number, or CNIC. If a user with the given email, phone, or CNIC already exists, the method throws a BadRequestException with the corresponding error message. Email validation, phone validation, CnicValidation, verification, email check, phone check, cnic check, 
 
   async userCheck (accountVerifyDto :VerifyDto):Promise<ResponseDto> {
@@ -80,8 +88,19 @@ throw new InternalServerErrorException(error);
       throw new InternalServerErrorException(error);
     }
   }
-      
-/////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Retrieve, get date after one month, date function, next month. For example, if the current date is April 28, 2023, then date would represent April 28, 2023 and endDate would represent May 31, 2023 (since the next month after April is May, and May has 31 days).
 
 
@@ -89,8 +108,20 @@ throw new InternalServerErrorException(error);
 const date = new Date();
 const endDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
       
-    
-//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Send sending object combining object as whole as API response, send object as response in API
 async propertiesOverview(){
   try{
@@ -129,7 +160,7 @@ async propertiesOverview(){
 }
 
         
-////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Following code are used to update a Profile entity in the database by incrementing the value of the freeToolCount property.
 
 
@@ -141,3 +172,49 @@ const RepoProfile =  queryRunner.manager.getRepository(Profile);
 //The second line updates the Profile entity by incrementing the value of the freeToolCount property by 1, and then saves the updated entity back to the database.  
           await RepoProfile.update({userId : +createSaveAllPdfDto.userId},{freeToolCount : old_count.freeToolCount + 1}) //'freeToolCount' is the element in 'Profile'entity
  }  
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Check if location area exists in the given longitude long and latitude lat, in the given radius, is within radius
+//loop iteration on a function, array push pushing, sending array as response, push results in array
+  @Get('/isAgencyWithinRadius')
+  isAgencyWithinRadius(@Query() isAgencyWithinRadiusDto : IsAgencyWithinRadiusDto) {
+    return this.dashboardService.isAgencyWithinRadius(isAgencyWithinRadiusDto);
+  }
+
+  async isAgencyWithinRadius(isAgencyWithinRadiusDto : IsAgencyWithinRadiusDto): Promise<any>{
+
+    
+  const promotionRepo = getRepository(Agency)
+  const result = await promotionRepo.createQueryBuilder('agency')
+  .select(['agency.latitude', 'agency.longitude', 'agency.id', 'agency.agencyName', 'agency.logo_Url', 'agency.address', 'agency.NoOfStaffs', 'createdByUser.phone'])
+  .leftJoin('agency.createdByUser', 'createdByUser')
+  .getMany()
+
+  let locations = []
+
+for (const agency of result) {
+ const aa = geolib.isPointWithinRadius(
+{ latitude:agency.latitude, longitude: agency.longitude},{ latitude: isAgencyWithinRadiusDto.latitude, longitude: isAgencyWithinRadiusDto.longitude}, +isAgencyWithinRadiusDto.radius);
+
+if(aa){
+  let propertyCount = await this.getAgencyPropertyCount(agency.id)
+  locations.push({ 
+    agencyId: agency.id,
+    agencyName: agency.agencyName,
+    agencyLogo: agency.logo_Url,
+    agencyAddress: agency.address,
+    agencyNoOfStaff: agency.NoOfStaffs,
+    latitude: agency.latitude,
+    longitude: agency.longitude,
+    phone: agency.createdByUser.phone,
+    propertyCount : propertyCount
+  })
+}}
+
+return locations
+  }
