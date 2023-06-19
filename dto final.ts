dbf203737052f07d1101041aa8e9282bb6569103
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-06-19 17:44:15 +0500

    xx

--- dto final.ts
+++ dto final.ts
@@ -74,3 +74,48 @@ url: string;
 
 
 }
+
+
+---------
+
+    export class CreatePropertyWalletProductStep1Dto {
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    projectTypeId: number
+
+    @ApiProperty()
+    @IsBoolean()
+    @IsNotEmpty()
+    NOC : boolean
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    description : string
+
+    @ApiProperty()
+    @IsPhoneNumber()
+    @IsNotEmpty()
+    ownerPhone : string
+    
+    @ApiProperty()
+    @IsEmail()
+    @IsNotEmpty()
+    ownerEmail : string
+    
+    @ApiProperty({ required: true })
+    @IsString()
+    fullName: string;
+
+    @ApiProperty({ required: true })
+    @IsEmail()
+    email: string
+  
+    @ApiProperty({ required: true })
+    @IsPhoneNumber()
+    phone: string
+}
+
+
