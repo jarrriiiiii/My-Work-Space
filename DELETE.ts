00134d6b40fb51089a1377b072e05f0de1ad4e2d
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-08-22 14:47:35 +0500

    Tagged

--- DELETE.ts
+++ DELETE.ts
@@ -1,4 +1,36 @@
-//soft delete by title, soft delete by id, hard delete by dto, soft delete by id, soft delete, remove by id, remove by dto
+//soft delete by id, soft delete, remove by id
+
+@noModulePermission()
+  @Delete('deleteListing/:id')
+  deleteListing(@Param('id') id: number){
+  return this.agencyService.deleteListing(+id);
+  }
+
+
+async deleteListing(id: number): Promise<any> {
+  const queryRunner = this.connection.createQueryRunner();
+  await queryRunner.connect();
+  await queryRunner.startTransaction();
+  try {
+    const listingRepo = queryRunner.manager.getRepository(Listing);
+    const findCheck = await listingRepo.findOne({ id });
+    if (findCheck.id) {
+      await listingRepo.softDelete({ id: id}) 
+    }else {
+      throw new BadRequestException(commonMessage.idNotFound)
+    }
+    await queryRunner.commitTransaction();
+    return { message: commonMessage.delete, data: null };
+  } catch (error) {
+    await queryRunner.rollbackTransaction();
+    throw new InternalServerErrorException(error);
+  } finally {
+    await queryRunner.release();
+  }
+}
+
+
+//soft delete by id, soft delete, remove by id
 
 @noModulePermission()
   @Delete('deleteManualNotification/:id') 
