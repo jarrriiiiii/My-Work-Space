3a0623699147117f2b52c0fba750ed710a593b24
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-09-04 16:36:44 +0500

    Tagged

--- Other/DTO.ts
+++ Other/DTO.ts
@@ -1,120 +1,117 @@
-export enum Type{
-    SaleQuotation = 'SaleQuotation',
-    SaleOrder = 'SaleOrder',
-    PaymentPlan = 'PaymentPlan',
-    Brochure = 'Brochure',
-    PostCreator = 'PostCreator'
+export enum Type {
+  SaleQuotation = "SaleQuotation",
+  SaleOrder = "SaleOrder",
+  PaymentPlan = "PaymentPlan",
+  Brochure = "Brochure",
+  PostCreator = "PostCreator",
 }
-
 
 export class CreateNotificationDto {
-    @ApiProperty()
-    @IsNumber()
-    userId : number
+  @ApiProperty()
+  @IsNumber()
+  userId: number;
 
-    @ApiProperty()
-    @IsString()
-    shortTitle : string
+  @ApiProperty()
+  @IsString()
+  shortTitle: string;
 
-    @ApiProperty()
-    @IsString()
-    notificationType : string
+  @ApiProperty()
+  @IsString()
+  notificationType: string;
 
-    @ApiProperty()
-    @IsString()
-    message : string
+  @ApiProperty()
+  @IsString()
+  message: string;
 
-    @ApiProperty()
-    @IsNumber()
-    createdBy : number
+  @ApiProperty()
+  @IsNumber()
+  createdBy: number;
 
-    @ApiProperty({required : false})
-    @IsOptional()
-    url : string
+  @ApiProperty({ required: false })
+  @IsOptional()
+  url: string;
 
-    @ApiProperty({required : false})
-    @IsOptional()
-    refId : string
+  @ApiProperty({ required: false })
+  @IsOptional()
+  refId: string;
 
-    @ApiProperty()
-    @IsOptional()
-    imageUrl : string
+  @ApiProperty()
+  @IsOptional()
+  imageUrl: string;
 
-    @ApiProperty({ type: 'string', format: 'binary', required: true })
-    freeTool: Express.Multer.File;
+  @ApiProperty({ type: "string", format: "binary", required: true })
+  freeTool: Express.Multer.File;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty()
-    @IsEnum([Type.SaleQuotation, Type.SaleOrder , Type.PaymentPlan, Type.Brochure,Type.PostCreator ])
-    status: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @IsEnum([
+    Type.SaleQuotation,
+    Type.SaleOrder,
+    Type.PaymentPlan,
+    Type.Brochure,
+    Type.PostCreator,
+  ])
+  status: string;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    projectTypeId: number
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  projectTypeId: number;
 
-    @ApiProperty()
-    @IsBoolean()
-    @IsNotEmpty()
-    NOC : boolean
+  @ApiProperty()
+  @IsBoolean()
+  @IsNotEmpty()
+  NOC: boolean;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    description : string
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  description: string;
 
-    @ApiProperty()
-    @IsPhoneNumber()
-    @IsNotEmpty()
-    ownerPhone : string
-    
-    @ApiProperty()
-    @IsEmail()
-    @IsNotEmpty()
-    ownerEmail : string
-    
-    @ApiProperty({ required: true })
-    @IsString()
-    fullName: string;
+  @ApiProperty()
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  ownerPhone: string;
 
-    @ApiProperty({ required: true })
-    @IsEmail()
-    email: string
-  
-    @ApiProperty({ required: true })
-    @IsPhoneNumber()
-    phone: string
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  ownerEmail: string;
 
-    /////Placing another Sub-Dto as an array in the dto. 'CreateHotListingSubDto' is written below
-    @ApiProperty({ type: [CreateHotListingSubDto] }) 
-    createHotListingSubDto: CreateHotListingSubDto[];
+  @ApiProperty({ required: true })
+  @IsString()
+  fullName: string;
 
+  @ApiProperty({ required: true })
+  @IsEmail()
+  email: string;
 
-//To show enum dropdown in the swagger
-    @ApiProperty({type: 'string',enum: InventoryStatus, required:false})
-    @IsOptional()
-    inVentoryType: InventoryStatus;
+  @ApiProperty({ required: true })
+  @IsPhoneNumber()
+  phone: string;
+
+  /////Placing another Sub-Dto as an array in the dto. 'CreateHotListingSubDto' is written below
+  @ApiProperty({ type: [CreateHotListingSubDto] })
+  createHotListingSubDto: CreateHotListingSubDto[];
+
+  //To show enum dropdown in the swagger
+  @ApiProperty({ type: "string", enum: InventoryStatus, required: false })
+  @IsOptional()
+  inVentoryType: InventoryStatus;
 }
 
+export class CreateHotListingSubDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  inventoryId: number;
 
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  saleCommission: number;
 
-
-
-export class CreateHotListingSubDto{
-
-    @ApiProperty()
-    @IsNotEmpty()
-    inventoryId: number;
-  
-    @ApiProperty()
-    @IsNumber()
-    @IsNotEmpty()
-    saleCommission: number;
-  
-    @ApiProperty({required : false})
-    @IsOptional()
-    isNegotiate : boolean;
-  
-  }
-  
+  @ApiProperty({ required: false })
+  @IsOptional()
+  isNegotiate: boolean;
+}
