c8eead02663e64a5a80ec9c9a5fd4b76de018d05
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2023-12-28 15:39:50 +0500

    Tagged

--- Other/Search and Filters.ts
+++ Other/Search and Filters.ts
@@ -1,3 +1,4 @@
+--------------------------------------------DTO FILE----------------------------------
 export class SearchGetUserManagementListDto {
     @ApiProperty({required : false})
     @IsOptional()
@@ -15,23 +16,115 @@ export class SearchGetUserManagementListDto {
     @IsOptional()
     phone : string
 
-//For Adding SORT BY Button
-//Also add the ENUM for this
+    //For Adding SORT BY Button
+    //Also add the ENUM for this
     @ApiProperty({ type: 'string', enum: SortByOrder, required: false })
     @IsOptional()
     sortByOrder: SortByOrder;
+
+  
     
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  projecSubtype: ReqStatus;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  projectTypeId: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  projectSubTypeId: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  inventoryTitle: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  minimumPrice: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  maximumPrice: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  minCommission: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  maxCommission: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  minlandSize: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  maxlandSize: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  landAreaId: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  bedRooms: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  washRooms: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  postedStartDate: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  postedEndDate: string;
+
+  @ApiProperty({ type: 'string', enum: InventoryStatus, required: false })
+  @IsOptional()
+  inVentoryType: InventoryStatus;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  city: string;
 }
 
 
-
+////Enums for additional use
 export enum SortByOrder {
   Ascending = 'Ascending',
   Descending = 'Descending',
   SortByLastMonth = 'SortByLastMonth',
 }
 
-----------------------------------------------------------------------------
+
+export enum ReqStatus {
+  Approved = 'Approved',
+  Pending = 'Pending',
+  Rejected = 'Rejected',
+  Removed = 'Removed',
+  Expired = 'Expired',
+}
+
+--------------------------------------------CONTROLLER FILE----------------------------------
   @Get('getUserManagementList')
   getUserManagementList(
     @Query('page', ParseIntPipe) page: number,
@@ -41,7 +134,7 @@ export enum SortByOrder {
   }
 
 
-----------------------------------------------------------------------------
+--------------------------------------------SERVICE FILE----------------------------------
   async getUserManagementList(page: number, limit: number, searchGetUserManagementListDto : SearchGetUserManagementListDto): Promise<ResponseDto> {
     try {
       const AdminUserAuthRepo = getRepository(AdminUserAuth);
@@ -52,7 +145,7 @@ export enum SortByOrder {
 
 
 
-//ELASTIC SEARCH
+/////////////////////////ELASTIC SEARCH
 if (searchAppUserDto.fullName) {
     Result.andWhere('LOWER(profile.fullName) like LOWER(:fullName)', {
       fullName: `%${searchAppUserDto.fullName}%`,
@@ -78,20 +171,7 @@ if (searchAppUserDto.phone) {
   }
 
 
-
-//NON ELASTIC SEARCH
-
-if (searchAppUserDto.minCommission && searchAppUserDto.maxCommission) {
-    Result.andWhere('HotListing.saleCommission BETWEEN :minCommission AND :maxCommission', { minCommission: searchAppUserDto.minCommission, maxCommission: searchAppUserDto.maxCommission })
-}
-
-  if (searchAppUserDto.minCommission && searchAppUserDto.maxCommission) {
-    Result.andWhere('HotListing.updatedAt BETWEEN :postedStartDate AND :postedEndDate',{ postedStartDate: moment(searchAppUserDto.postedStartDate) , postedEndDate: moment(searchAppUserDto.postedEndDate) })
-}
-
-  if (searchAppUserDto.startDate && searchAppUserDto.endDate) {
-    Result.andWhere('user.createdAt BETWEEN :start AND :end', { start: searchAppUserDto.startDate, end: searchAppUserDto.endDate })
-}
+//////////////////////////NON ELASTIC SEARCH/FILTERS
 
   if (searchAppUserDto.isVerified == 'true') {
     Result.andWhere('user.isVerified = :check', { check:searchAppUserDto.isVerified })
@@ -101,17 +181,37 @@ if (searchAppUserDto.minCommission && searchAppUserDto.maxCommission) {
     Result.andWhere('user.isVerified = :check', { check:searchAppUserDto.isVerified })
 }
 
-
-
-        
-//NON ELASTIC SEARCH
-//Conditional search for Quick filters
 if (getProjectTypeAndSubTypeDto.projectTypeId) {
     Result.andWhere('inventory.projectTypeId = :projectTypeId', { projectTypeId: getProjectTypeAndSubTypeDto.projectTypeId });
   
-    if (getProjectTypeAndSubTypeDto.projectSubTypeId) {
+if (getProjectTypeAndSubTypeDto.projectSubTypeId) {
       Result.andWhere('inventory.projectSubTypeId = :projectSubTypeId', { projectSubTypeId: getProjectTypeAndSubTypeDto.projectSubTypeId });
+}
+    
+if (getProjectTypeAndSubTypeDto.bedRooms ) {
+      Result.andWhere('inventory.bedRooms = :bedRooms', { bedRooms :getProjectTypeAndSubTypeDto.bedRooms })
     }
+    
+if (getProjectTypeAndSubTypeDto.washRooms) {
+      Result.andWhere('inventory.washRooms = :washRooms', { washRooms :getProjectTypeAndSubTypeDto.washRooms })
+    }
+
+/////////////////////////RANGE BASED QUICK FILTERS 
+
+    if (searchAppUserDto.minCommission && searchAppUserDto.maxCommission) {
+    Result.andWhere('HotListing.saleCommission BETWEEN :minCommission AND :maxCommission', { minCommission: searchAppUserDto.minCommission, maxCommission: searchAppUserDto.maxCommission })
+}
+
+  if (searchAppUserDto.minCommission && searchAppUserDto.maxCommission) {
+    Result.andWhere('HotListing.updatedAt BETWEEN :postedStartDate AND :postedEndDate',{ postedStartDate: moment(searchAppUserDto.postedStartDate) , postedEndDate: moment(searchAppUserDto.postedEndDate) })
+}
+
+  if (searchAppUserDto.startDate && searchAppUserDto.endDate) {
+    Result.andWhere('user.createdAt BETWEEN :start AND :end', { start: searchAppUserDto.startDate, end: searchAppUserDto.endDate })
+
+}
+    
+
     if (getProjectTypeAndSubTypeDto.minlandSize && getProjectTypeAndSubTypeDto.maxlandSize  && getProjectTypeAndSubTypeDto.landAreaId) {
       Result.andWhere('inventory.landAreaId = :landAreaId', { landAreaId: getProjectTypeAndSubTypeDto.landAreaId })
       Result.andWhere('inventory.landSize BETWEEN :minlandSize AND :maxlandSize', { minlandSize: getProjectTypeAndSubTypeDto.minlandSize, maxlandSize: getProjectTypeAndSubTypeDto.maxlandSize })
@@ -119,18 +219,12 @@ if (getProjectTypeAndSubTypeDto.projectTypeId) {
     if (getProjectTypeAndSubTypeDto.minimumPrice && getProjectTypeAndSubTypeDto.maximumPrice ) {
       Result.andWhere('inventory.price BETWEEN :minPrice AND :maxPrice', { minPrice: getProjectTypeAndSubTypeDto.minimumPrice, maxPrice: getProjectTypeAndSubTypeDto.maximumPrice })
     }
-    if (getProjectTypeAndSubTypeDto.bedRooms ) {
-      Result.andWhere('inventory.bedRooms = :bedRooms', { bedRooms :getProjectTypeAndSubTypeDto.bedRooms })
-    }
-    if (getProjectTypeAndSubTypeDto.washRooms) {
-      Result.andWhere('inventory.washRooms = :washRooms', { washRooms :getProjectTypeAndSubTypeDto.washRooms })
-    }
+
+    
   }
       
 
-
-        
-//For Sort By: (ASC, DESC) Button 
+///////////////For Sort By: (ASC, DESC) Button 
         //DTO Entry is saved above in the DTO section
         
 if (searchAssignLoProjectForAppDto.sortByOrder && searchAssignLoProjectForAppDto.sortByOrder === SortByOrder.Ascending) {
@@ -139,7 +233,7 @@ if (searchAssignLoProjectForAppDto.sortByOrder && searchAssignLoProjectForAppDto
         loProjectResult.orderBy('assignInventoryForLounge.id', 'DESC');
       }
         
-//For Sort By: Last Month only
+/////////////////For Sort By: Last Month only
 
       if (getAllReviewForAgencyFilterDto.sortByOrder && getAllReviewForAgencyFilterDto.sortByOrder === SortByOrder.SortByLastMonth) {
        
@@ -151,7 +245,11 @@ if (searchAssignLoProjectForAppDto.sortByOrder && searchAssignLoProjectForAppDto
         agencyReviewResult.orderBy('agencyReview.createdAt', 'DESC');
       }
         
-        const totalItems = await AdminUserAuthResult.getCount();
+       
+
+
+
+ const totalItems = await AdminUserAuthResult.getCount();
     
     const pg = await paginate<AdminUserAuth>(AdminUserAuthResult, {
       limit,
