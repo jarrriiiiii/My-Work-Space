c7aad917bc3dd5204bdf216c3526b251e275143c
jarrriiiiii <79092437+jarrriiiiii@users.noreply.github.com>
2024-03-30 15:31:06 +0500

    Tagged

--- DELETE.ts
+++ DELETE.ts
@@ -104,3 +104,76 @@ return this.serviceXXXXX.DeletePWInventoryPlot(+id);
 
 
 
+
+
+
+
+---------------------------------------------------------------------------------------
+
+  @hasRole(RoleType.agentOwner, RoleType.agentManager)
+  @Delete('/delete-multi-user')
+  deleteMultipleUser(@Body() deleteMultipleUserDto: DeleteMultipleUserDto) {
+    return this.authService.deleteMultipleUser(deleteMultipleUserDto);
+  }
+
+
+  
+
+    async deleteMultipleUser(deleteMultipleUserDto: DeleteMultipleUserDto) {
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const userRepo = await queryRunner.manager.getRepository(User);
+      const profRepo = await queryRunner.manager.getRepository(Profile);
+
+      for (let i = 0; i < deleteMultipleUserDto.userId.length; i++) {
+        const userData = await userRepo
+          .createQueryBuilder('u')
+          .leftJoinAndSelect('u.role', 'role')
+          .leftJoinAndSelect('u.profile', 'profile')
+          .leftJoinAndSelect('profile.agency', 'agency')
+          .where('u.id = :id', { id: deleteMultipleUserDto.userId[i] })
+          .getOne();
+
+        const ownerId = userData.profile.agency.createdBy;
+
+        if (userData.role.title == RoleType.agentManager) {
+          const staffData = await userRepo
+            .createQueryBuilder('u')
+            .where('u.createdBy = :createdBy', {
+              createdBy: deleteMultipleUserDto.userId[i],
+            })
+            .getMany();
+          for (const x of staffData) {
+            await userRepo.update(
+              { id: x.id },
+              { createdBy: ownerId, updatedBy: ownerId },
+            );
+          }
+        }
+
+        await profRepo.softDelete({ userId: deleteMultipleUserDto.userId[i] });
+        await userRepo.softDelete({ id: deleteMultipleUserDto.userId[i] });
+
+        const msg1 = `Dear ${userData.profile.fullName}, we regret to inform you that you have been removed from ${userData.profile.agency.agencyName} by your agency owner. Please contact your agency owner for further queries.`;
+
+        if (userData?.email) {
+          await this.emailService.sentInformation(userData.email, msg1);
+        }
+        if (userData?.phone) {
+          await this.sendMessage(msg1, userData.phone);
+        }
+      }
+
+      await queryRunner.commitTransaction();
+      return { message: commonMessage.delete, data: null };
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(error);
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+
